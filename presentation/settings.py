e1ee81a26ab293b0d11737056afedfb98d268bc3
GlennJo <g.j@live.se>
2026-03-21 14:39:54 +0100

    Startar projekt

--- presentation/settings.py
+++ presentation/settings.py
@@ -1,13 +1,26 @@
+import os
 from pathlib import Path
+if os.path.isfile('env.py'):
+    import env
+
+BASE_DIR             = Path(__file__).resolve().parent.parent
+SECRET_KEY           = 'django-insecure-*l%dh@7-ohlq(+7pp^57)_nx(n*_8o18*o%lt#y2673i0ok)=='
+ROOT_URLCONF         = 'presentation.urls'
+WSGI_APPLICATION     = 'presentation.wsgi.application'
+DEBUG                = True
+LANGUAGE_CODE        = 'en-us'
+TIME_ZONE            = 'UTC'
+USE_I18N             = True
+USE_TZ               = True
+CSRF_TRUSTED_ORIGINS = ['']
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    'localhost',
+    '127.0.0.1',
 
-SECRET_KEY = 'django-insecure-*l%dh@7-ohlq(+7pp^57)_nx(n*_8o18*o%lt#y2673i0ok)=='
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -17,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django_browser_reload",
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
@@ -30,8 +44,6 @@ MIDDLEWARE = [
      "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
-ROOT_URLCONF = 'presentation.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -39,24 +51,20 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'presentation.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME'  : BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -70,26 +78,21 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+# Static files settings for local development
+STATIC_URL       = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT      = os.path.join(BASE_DIR, 'staticfiles')
 
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
+WHITENOISE_MAX_AGE = 0  # Set to 0 during development to avoid caching
 
-LANGUAGE_CODE = 'en-us'
+# Cloudinary will handle the media
+MEDIA_URL  = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-# === Media - Kodad ===
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-# === Media - Via hemsida ===
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# Media files (Cloudinary)
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY'   : os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
 
